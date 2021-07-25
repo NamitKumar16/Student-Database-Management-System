a946fe9a93a9092cd0c20568a3af59df1f28705b
NamitKumar16 <69860507+NamitKumar16@users.noreply.github.com>
2021-07-25 20:21:40 +0530

    MVC

---
+++ database/migrations/2021_07_25_143016_create_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('Name');
+            $table->string('RollNo');
+            $table->bigInteger('Contact');
+            $table->string('Email');
+            $table->string('Year');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
