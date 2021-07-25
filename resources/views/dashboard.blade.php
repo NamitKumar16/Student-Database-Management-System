a5423d5b4b5455ad96334fdb5eb09012cef9387b
NamitKumar16 <69860507+NamitKumar16@users.noreply.github.com>
2021-07-25 17:19:50 +0530

    Worked on Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,37 @@
 <x-layout>
-    <h1 class="font-bold text-3xl text-center text-purple-600">Hello</h1>
+    <style>
+        table, tr, td, th {
+            border-width: 2px;
+            color: #4a5568;
+            padding: 12px;
+        }
+        table{
+            width: 100%
+        }
+        .center{
+            margin-left: auto;
+            margin-right: auto;
+        }
+    </style>
+    <div class=" text-center mt-5">
+        <div>
+            <table class="center">
+                <tr>
+                    <th>Name</th>
+                    <th>Roll No</th>
+                    <th>Contact</th>
+                    <th>Email</th>
+                    <th>Year</th>
+                </tr>
+                <tr>
+                    <td>Namit Kumar</td>
+                    <td>19csu185</td>
+                    <td>8130216394</td>
+                    <td>kumarnamit58@gmail.com</td>
+                    <td>2nd</td>
+                </tr>
+            </table>
+        </div>
+    </div>
 </x-layout>
 
