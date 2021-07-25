7d0d5a2c449c6aa61684f47be35569a90c144852
NamitKumar16 <69860507+NamitKumar16@users.noreply.github.com>
2021-07-25 13:44:45 +0530

    Updated CSS on landing pages

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,10 +6,20 @@
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>
 
+<head>
+    <style>
+        #header {
+            background: black;
+            font-family: Arial, Helvetica, sans-serif;
+            height: 55px;
+        }
+    </style>
+</head>
+
 <body>
-<div class="items-center md:mt-0 mt-8 text-right">
+<div class="items-center md:mt-0 text-right" id="header">
     @auth()
-        <span class="font-bold uppercase text-right">Welcome, {{ auth()->user()->name }}!</span>
+        <span class="font-bold uppercase text-right" style="color: white">Welcome, {{ auth()->user()->name }}!</span>
         <form method="post" action="/logout" class="font-semibold text-blue-500 ml-6">
             @csrf
 
@@ -17,8 +27,9 @@
 
         </form>
     @else
-        <a href="/register" class="font-bold text-red-600 uppercase ">Register</a>
-        <a href="/login" class="font-bold ml-6 text-blue-600 uppercase">Login</a>
+        <br>
+        <a href="/register" class="font-bold text-red-600 uppercase">Register</a>
+        <a href="/login" class="font-bold ml-6 text-blue-600 uppercase mr-4">Login</a>
     @endauth
     {{ $slot }}
 </div>
